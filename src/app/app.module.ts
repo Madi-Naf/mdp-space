c5c0c8d376b80d11838d9788fd98ad00c5ac8684
Madi-Naf <onplayer6@gmail.com>
2018-10-08 17:41:05 +0200

    landing page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes,Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
@@ -20,24 +16,12 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LandingPageComponent
-=======
-    MainNavComponent,
     ContactComponent,
     HomeComponent,
     MenuComponent
->>>>>>> developement
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
