7e36dad38d6e6a07353f19fe13a9b05e81189454
Damien Heulin <damienheulin87@gmail.com>
2018-10-09 10:32:51 +0200

    Création du footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes,Router} from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: 'Home', component: HomeComponent},
@@ -15,11 +17,13 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
